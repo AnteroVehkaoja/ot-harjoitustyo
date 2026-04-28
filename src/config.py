14d87ab7396ab08f75b119054e107efac3029dae
AnteroVehkaoja <apsoluutti@gmail.com>
2026-04-28 22:10:01 +0300

    remove .env

--- src/config.py
+++ src/config.py
@@ -1,22 +1,10 @@
-import os
-from dotenv import load_dotenv
+CELL_SIZE =  48 
+HEIGHT =  10 
+WITDTH =  10 
+NUMBEROFMINES =  22
+GEN_GRID = "yes" # yes or no
+# yes or no, this is not entirely functional in the sense that it doesnt know all solving methods yet,
+#so if it is enabled it will filter out boards that are solvable
+# because the solve function is still stupid
 
-dirname = os.path.dirname(__file__)
 
-try:
-    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
-except FileNotFoundError:
-    pass
-
-
-CELL_SIZE = os.getenv("CELL_SIZE")  or 48
-HEIGHT = os.getenv("HEIGHT") or 10
-WITDTH = os.getenv("WITDTH") or 10
-NUMBEROFMINES = os.getenv("NUMBEROFMINES") or 15
-GEN_GRID = os.getenv("GENERATE_GUESLESS_GRID") or "error"
-
-GEN_GRID = str(GEN_GRID)
-CELL_SIZE = int(CELL_SIZE)
-HEIGHT = int(HEIGHT)
-WITDTH = int(WITDTH)
-NUMBEROFMINES = int(NUMBEROFMINES)
